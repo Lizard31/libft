2a75e371501114f118007e2f048afea89c52698d
Tudor Bordianu <tbordian@3-B-11.42heilbronn.de>
2025-10-04 23:06:22 +0200

    fixed get next line

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,54 +6,12 @@
 /*   By: tbordian <tbordian@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 13:42:18 by tbordian          #+#    #+#             */
-/*   Updated: 2025/08/04 15:45:42 by tbordian         ###   ########.fr       */
+/*   Updated: 2025/10/04 23:05:12 by tbordian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-size_t	ft_strlen(const char *s)
-{
-	size_t	len;
-
-	len = 0;
-	while (s[len])
-		len++;
-	return (len);
-}
-
-char	*ft_strchr(const char *s, int c)
-{
-	while (*s)
-	{
-		if (*s == (char)c)
-			return ((char *)s);
-		s++;
-	}
-	if ((char)c == '\0')
-		return ((char *)s);
-	return (0);
-}
-
-char	*ft_strdup(const char *s1)
-{
-	size_t	len;
-	char	*dup;
-	size_t	i;
-
-	len = ft_strlen(s1);
-	dup = (char *)malloc(len + 1);
-	if (!dup)
-		return (NULL);
-	i = 0;
-	while (s1[i])
-	{
-		dup[i] = s1[i];
-		i++;
-	}
-	dup[i] = '\0';
-	return (dup);
-}
+#include "../libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -77,29 +35,4 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	}
 	str[i] = '\0';
 	return (str);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	char	*new;
-	int		a;
-	int		b;
-
-	a = 0;
-	b = 0;
-	new = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
-	if (!new)
-		return (NULL);
-	while (s1[a])
-	{
-		new[a] = s1[a];
-		a++;
-	}
-	while (s2[b])
-	{
-		new[a + b] = s2[b];
-		b++;
-	}
-	new[a + b] = '\0';
-	return (new);
 }
